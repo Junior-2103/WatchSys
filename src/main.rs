0ad617e7fede3793721d54a17aae9c84c0f552a9
Junior-2103 <jjosewilker45@proton.me>
2026-03-20 19:35:16 -0300

    ADD: View Disk space

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use sysinfo::{System,Networks};
+use sysinfo::{System,Disks,Networks};
 use std::process::Command;
+// use std::collections::HashMap;
 
 #[derive(Debug)]
 struct WiFi{
@@ -8,10 +9,18 @@ struct WiFi{
     transmitted: u64,
 }
 
+struct Disk{
+    name: String,
+    total: f64,
+    used: f64
+}
+
 struct Info{
     cpu_usage: f32,
     ram_usage: f64,
+    disk_usage: Vec<Disk>,
     wifi_usage: Vec<WiFi>,
+    // top_process: HashMap<String, i32>,
 }
 
 fn main() {
@@ -23,17 +32,22 @@ fn main() {
 
         let unit = GIB;
         
+        let mut disks = Disks::new_with_refreshed_list();
         let mut network = Networks::new_with_refreshed_list();
         
+        
+        print!("{:?}\n",disks.first());
         loop {
-            let mut wifi = Vec::new();
+            let mut wifi_vec = Vec::new();
+            let mut disks_vec = Vec::new();
 
             sys.refresh_all();
             network.refresh(true);
+            disks.refresh(true);
             
             for net in network.list(){
                 let net_info = net.1;
-                wifi.push(
+                wifi_vec.push(
                     WiFi {
                         name: net.0.to_string(),
                         received: net_info.received(),
@@ -41,11 +55,23 @@ fn main() {
                     }
                 );
             }
+
+            for disk in disks.list(){
+                let disk_usage = disk.total_space().saturating_sub(disk.available_space());
+                disks_vec.push(
+                    Disk {
+                        name: disk.name().to_string_lossy().to_string(),
+                        total: disk.total_space() as f64 / unit,
+                        used: disk_usage as f64 / unit,
+                    }
+                );
+            }
             
             let info = Info{
                 cpu_usage:sys.global_cpu_usage(),
                 ram_usage:sys.used_memory() as f64 / unit,
-                wifi_usage:wifi,
+                disk_usage: disks_vec,
+                wifi_usage:wifi_vec,
             };
 
             println!("Uso da cpu: {:.2}%",info.cpu_usage);
@@ -55,6 +81,10 @@ fn main() {
                 if wifi.received != 0 && wifi.transmitted != 0 {
                     println!("{} - Recebido: {:?} B/s Enviado: {:?} B/s",wifi.name,wifi.received,wifi.transmitted);
                 }
+            }
+
+            for disk in info.disk_usage {
+                println!("{} - {:.2} / {:.2}",disk.name,disk.used,disk.total)
             }
             
 
